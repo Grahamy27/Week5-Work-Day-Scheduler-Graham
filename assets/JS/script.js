87b4b41d3421ba882dddc47ba30ca141594a88e4
Zeus <grahamy2711@yahoo.com>
2023-11-01 10:27:04 +0000

    stored time boxes in HTML

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,5 @@
+var dateTime = $('#currentDay');
+var now = dayjs()
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
